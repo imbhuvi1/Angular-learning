75b25edb45945137cf4f07b3af89f01b9ce4dd51
imbhuvi1 <bhuvneshsingh292@gmail.com>
2026-05-27 18:22:28 +0530

    Feat. Added model file for todoItems, modified todo.component.html and todo.componenet.ts and added todo component

---
+++ src/app/components/todo-items/todo-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-items',
+  templateUrl: './todo-items.component.html',
+  styleUrls: ['./todo-items.component.scss']
+})
+export class TodoItemsComponent {
+
+}
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal} from '@angular/core';
+import { TodosService } from '../services/todos.service';
+import { Todo } from '../model/todo.type';
 
 @Component({
   selector: 'app-todos',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent {}
+
+//to use ngOnInit() we have implemented the OnInit interface
+export class TodosComponent implements OnInit{
+  todoService = inject(TodosService);
+
+  //creating a signal of empty array later it will be set in the ngOnInit()
+  todoItems = signal<Array<Todo>>([]);
+  //it will trigger whenever this component initiated or the first time it comes into view
+  ngOnInit(): void {
+      console.log(this.todoService.todoItems);
+      this.todoItems.set(this.todoService.todoItems);
+  }
+}
